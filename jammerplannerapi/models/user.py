37ca46ebde9e92f109e258c4cb791f610294c056
Phantom-Farmer <joeljdahl@gmail.com>
2023-02-14 21:22:27 -0600

    started on fixtures

--- jammerplannerapi/models/user.py
+++ jammerplannerapi/models/user.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class User(models.Model):
 
+    name = models.CharField(max_length=50)
     uid = models.CharField(max_length=50)
     image_url = models.URLField(max_length=250)
     email = models.EmailField(max_length=250)
