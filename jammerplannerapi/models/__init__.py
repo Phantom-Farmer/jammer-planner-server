06095ab4b7b2f4c7b6c919caf6df8e8fa6505cb3
Phantom-Farmer <joeljdahl@gmail.com>
2023-02-14 21:06:21 -0600

    whoops, had forgotten my init for models

---
+++ jammerplannerapi/models/__init__.py
@@ -0,0 +1,7 @@
+from .user import User
+from .band import Band
+from .song import Song
+from .set import Set
+from .set_song import Set_Song
+from .rehearsal import Rehearsal
+from .comment import Comment
